7a115dcd4e82347543d45f95ea8dd17043b77fc2
Pedro Marques <pedromcm23@gmail.com>
2026-07-26 00:59:29 +0100

    fix: use correct datasource key in prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@prisma/config'
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
-  migrate: {
+  datasource: {
     url: process.env.POSTGRES_URL_NON_POOLING || process.env.POSTGRES_PRISMA_URL || process.env.DATABASE_URL,
   }
 })
